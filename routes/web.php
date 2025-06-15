0e25d1086876c3196971e6735b6a84f8cba88d13
Eduer Pallares <eduer.pallares@zalvadora.com>
2025-06-14 21:47:34 -0500

    🚧 Inicia implementacion: Chirps
    Agrega estructura base, modelo, controlador y ruta para crear y administrar chirps

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('chirps', [ChirpController::class, 'index'])->name('chirps');
+});
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::middleware(['auth'])->group(function () {
+    Route::view('profile', 'profile')->name('profile');
+});
 
 require __DIR__.'/auth.php';
