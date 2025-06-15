91d4022380d836dd798c1d635b8758d7cbe1b567
Eduer Pallares <eduer.pallares@zalvadora.com>
2025-06-15 12:13:12 -0500

    🌱 Agrega soporte para fábricas de modelos de Chirp
    Se incluye la clase ChirpFactory para facilitar la creación de instancias de Chirp en pruebas. Además, se añade el uso de HasFactory en el modelo Chirp para integrar la funcionalidad de fábricas.

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -12,6 +13,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Chirp extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'message',
     ];
